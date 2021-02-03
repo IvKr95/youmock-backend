dc30f4b74d537ab41c6aacd805ec3668b00d92f8
IvKr95 <cold67@mail.ru>
2021-02-03 13:18:27 +0300

    update

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import {NestExpressApplication} from "@nestjs/platform-express";
-import { AppModule } from './modules/app.module';
+import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 8080);
 }
 bootstrap();
